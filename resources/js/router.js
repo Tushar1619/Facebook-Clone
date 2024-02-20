da19530e320db811c9e96cf52386fb09c2d2b52b
Tushar Rai <tusharrai132@gmail.com>
2024-02-20 13:35:45 +0530

    User profile frontend error

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import NewsFeed from "./views/NewsFeed.vue";
-// import UserShow from "./views/Users/Show.vue";
+import Show from "./views/Users/Show.vue";
 
 const routes = [
     { path: "/", name: "home", component: NewsFeed },
-    // { path: "/users/:userId", name: "user.show", component: UserShow },
+    { path: "/users/:userId", name: "user.show", component: Show },
 ];
 
 const router = createRouter({
---
+++ resources/js/views/Users/Show.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full flex flex-col items-center">
+    <div class="w-full relative mb-8">
+      <div class="w-full h-64 overflow-hidden bg-green-500">
+        <img
+          src="https://c0.wallpaperflare.com/preview/542/56/997/orange-black-and-green-mechanical-keyboard.jpg"
+          alt="user background image"
+          class="object-cover w-full h-full"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Post from "../../components/Post.vue";
+
+import { onMounted, ref } from "vue";
+
+const user = ref([]);
+const loading = ref(true);
+
+onMounted(() => {
+  axios
+    .get("/api/users/" + this.$route.params.userId)
+    .then((res) => {
+        user.value = res.data;
+      loading.value = false;
+    })
+    .catch((err) => {
+        console.log('Enable to fetch the user from the server',err);
+      loading.value = false;
+    });
+});
+</script>
+
+<style></style>
