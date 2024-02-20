e768b6da38a7e0bfe038eebff3e5862280f7df5c
Tushar Rai <tusharrai132@gmail.com>
2024-02-20 15:06:44 +0530

    User posts Test

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserPostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function(){
 
-Route::apiResource('posts',PostController::class);
-Route::apiResource('users',UserController::class);
+Route::apiResource('/posts',PostController::class);
+Route::apiResource('/users',UserController::class);
+Route::apiResource('/users/{user}/posts',UserPostController::class);
 
 });
--- tests/Feature/UserCanViewProfileTest.php
+++ tests/Feature/UserCanViewProfileTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -19,21 +20,56 @@ class UserCanViewProfileTest extends TestCase
 
         // $posts = Post::factory()->create();
 
-        $response = $this->get('/api/users/'.$user->id);
+        $response = $this->get('/api/users/' . $user->id);
 
         $response->assertStatus(200)
-        ->assertJson([
-            'data'=>[
-                'type'=>'users',
-                'user_id'=>$user->id,
-                'attributes'=>[
-                    'name'=>$user->name,
-                ]
+            ->assertJson([
+                'data' => [
+                    'type' => 'users',
+                    'user_id' => $user->id,
+                    'attributes' => [
+                        'name' => $user->name,
+                    ]
                 ],
-                'links'=>[
-                    'self'=>url('/users'.$user->id)
+                'links' => [
+                    'self' => url('/users' . $user->id)
                 ],
             ]);
-        
+    }
+
+    public function testAUserCanFetchPostsForAProfile()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs($user = User::factory()->create(), 'api');
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->get('/api/users/' . $user->id . '/posts');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    [
+                        'data' => [
+                            'type' => 'posts',
+                            'post_id' => $post->id,
+                            'attributes' => [
+                                'body' => $post->body,
+                                'image' => $post->image,
+                                'posted_at' => $post->created_at->diffForHumans(),
+                                'posted_by' => [
+                                    'data' => [
+                                        'attributes' => [
+                                            'name' => $user->name,
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'links' => [
+                            'self' => url('/posts/'.$post->id),
+                        ]
+                    ]
+                ]
+            ]);
     }
 }
